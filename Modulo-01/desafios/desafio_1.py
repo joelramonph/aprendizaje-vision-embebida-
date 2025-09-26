427ea8b4f92bc5a4cd20bff1cbd0a5e1b91611f4
Joel <joelramonph@gmail.com>
2025-09-26 13:21:42 -0400

    se agrego directorio desafio y el reto para ver el video en escala de grises

--- Modulo-01/desafios/desafio_1.py
+++ Modulo-01/desafios/desafio_1.py
@@ -9,15 +9,15 @@ if not cap.isOpened():
 
 while True:
     ret, frame = cap.read()
-    
-    
+    cv2.putText(frame, "En vivo", (10, 50), cv2.FONT_HERSHEY_SIMPLEX,1, (0, 255, 0), 2, cv2.LINE_AA)
+    frameGris = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     if not ret:
         print("No se pudo recibir el frame. Saliendo...")
         break
 
 
-    cv2.imshow("Mi camra en vivo", frame)
+    cv2.imshow("Mi camra en vivo", frameGris)
     if cv2.waitKey(1) == ord("q"):
         break
 
